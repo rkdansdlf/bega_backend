9fc44096abc7f7efd472d6a0fa498ca23d891603
rkdansdlf <ansdlf000616e@gmail.com>
2026-02-19 04:02:59 +0900

    Update FollowBlockVerificationTest.java

--- BEGA_PROJECT/src/test/java/com/example/auth/service/FollowBlockVerificationTest.java
+++ BEGA_PROJECT/src/test/java/com/example/auth/service/FollowBlockVerificationTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
@@ -22,6 +23,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
+@ActiveProfiles("local")
 @Transactional
 class FollowBlockVerificationTest {
 
